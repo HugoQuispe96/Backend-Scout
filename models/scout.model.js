428934665be13d421d57f45ae47ee87944ffb40f
HugoQuispe96 <hugo.quispe96@outlook.es>
2020-04-01 03:32:40 -0500

    actualizar y eliminar scout, jefe y unidad

--- models/scout.model.js
+++ models/scout.model.js
@@ -1,23 +1,22 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 var esquema= new Schema({
-    id:{type:String,required:true},
     nombre:{type:String,required:true},
-    apellidos:{type:String,required:true},
-    fechaNacimiento:{type:Date,required:true},
+    apellido:{type:String,required:true},
+    fecha_nacimiento:{type:Date,required:true},
     correo:{type:String,required:true},
     unidad:{type:String,required:true},
     direccion:{type:String,required:true},
     celular:{type:String,required:true},
-    nombreEmergencia:{type:String,required:true},
-    celularEmergencia:{type:String,required:true},
+    nombre_emergencia:{type:String,required:true},
+    celular_emergencia:{type:String,required:true},
     barrio:{type:String},
     telefono:{type:String},
     ocupacion:{type:String},
-    estado:{type:Boolean, default: false},
-    inscrito:{type:Boolean, default: false},
-    nombreAcudiente:{type:String},
-    celularAcudiente:{type:String},
+    activo:{type:String},
+    inscrito_csa:{type:String},
+    nombre_acudiente:{type:String},
+    celular_acudiente:{type:String},
 });
 
 module.exports=mongoose.model('Scout',esquema);
--- routes/api/unidad.js
+++ routes/api/unidad.js
@@ -7,78 +7,30 @@ router.get('/',(req,res,next)=>{
     console.log("funciona");
 });
 
-router.get('/listar', Verificar.VerificarToken, async(req, res) => {
-    try {
-      const listaDb = await Unidad.find();
-      res.json(listaDb);
-    } catch (error) {
-      return res.status(400).json({
-        mensaje: 'Ocurrio un error',
-        error
-      })
-    }
-  });
-
-router.post('/nuevo', Verificar.VerificarToken, async(req, res) => {
-    const body = req.body;
-    try {
-      const unidadDB = await Unidad.create(body);
-      res.status(200).json(unidadDB); 
-    } catch (error) {
-      return res.status(500).json({
-        mensaje: 'Ocurrio un error',
-        error
-      })
-    }
-  });
-
-router.get('/listar/:id', Verificar.VerificarToken, async(req, res) => {
-    const _id = req.params.id;
-    try {
-      const unidadDB = await Unidad.findOne({_id});
-      res.json(unidadDB);
-    } catch (error) {
-      return res.status(400).json({
-        mensaje: 'Ocurrio un error',
-        error
-      })
-    }
-  });
-
-router.delete('/eliminar/:id', Verificar.VerificarToken, async(req, res) => {
-    const _id = req.params.id;
-    try {
-        const unidadDB = await Unidad.findByIdAndDelete({_id});
-        if(!unidadDB){
-        return res.status(400).json({
-            mensaje: 'No se encontró el id indicado',
-            error
-        })
+router.post('/Nuevo', Verificar.VerificarToken,(req,res,next)=>{
+    let datos=req.body;
+    let p=new Unidad(datos);
+    p.save((err,nuevo)=>{
+        if(err){
+            res.status(300).json({nuevo:[],error:err,estado:'fail'});
         }
-        res.json(unidadDB);  
-    } catch (error) {
-        return res.status(400).json({
-        mensaje: 'Ocurrio un error',
-        error
-        })
-    }
+        if(!nuevo){
+            res.status(302).json({error:'que paso??',estado:'fail'});
+        }
+        return res.status(200).json({nuevo:nuevo,estado:'ok'});
+    });
 });
 
-router.put('/actualizar/:id', Verificar.VerificarToken, async(req, res) => {
-    const _id = req.params.id;
-    const body = req.body;
-    try {
-      const unidadDB = await Unidad.findByIdAndUpdate(
-        _id,
-        body,
-        {new: true});
-      res.json(unidadDB);  
-    } catch (error) {
-      return res.status(400).json({
-        mensaje: 'Ocurrio un error',
-        error
-      })
-    }
-  });
+router.get('/Listar',Verificar.VerificarToken,(req,res,next)=>{        
+    Unidad.find({},(err,query)=>{
+        if(err){
+            res.status(300).json({lista:[],error:err,estado:'fail'});
+        }
+        if(!query){
+            res.status(302).json({lista:[],error:'que??',estado:'fail'});
+        }
+        return res.status(200).json({estado:1,lista:query,estado:'ok'});
+    });   
+});
 
 module.exports = router;
